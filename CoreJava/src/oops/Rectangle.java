866f9511ded0c94acb2c6d466f10fe1c025ab259
aayush105 <sthaaayush105@gmail.com>
2023-06-22 19:54:20 +0545

    lab 8:OOP(class and object)

---
+++ CoreJava/src/oops/Rectangle.java
@@ -0,0 +1,15 @@
+package oops;
+
+public class Rectangle {
+
+	int length;
+	int breadth;
+	
+	void printArea() {
+		int area = length * breadth;
+		System.out.println("Area : "+ area);
+	}
+	
+	
+}
+
