0781aa7d13a9e6c0ca1594e9ee6f5b4dfe3aeeb9
aayush105 <sthaaayush105@gmail.com>
2023-06-24 22:22:24 +0545

    Practice level 1

---
+++ CoreJava/src/ClassObjectPractice/Test4.java
@@ -0,0 +1,13 @@
+package ClassObjectPractice;
+
+public class Test4 {
+public static void main(String[] args) {
+	Triangle t = new Triangle();
+	t.a = 3;
+	t.b = 4;
+	t.c = 5;
+	
+	t.getPerimeter();
+	t.getArea();
+}
+}
