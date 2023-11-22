bb14d5cf3b9232ce3ab22a01ecf26c7901537ed5
aayush105 <sthaaayush105@gmail.com>
2023-11-22 14:46:43 +0545

    Spring: work on edit and delete for employee model

--- springproject3pm/src/main/java/com/bway/springproject/controller/EmployeeController.java
+++ springproject3pm/src/main/java/com/bway/springproject/controller/EmployeeController.java
@@ -37,7 +37,7 @@ public class EmployeeController {
 	@GetMapping("/empList")
 	public String empList(Model model) {
 
-		model.addAttribute("eList", empService.getAllEmps());
+		model.addAttribute("empList", empService.getAllEmps());
 
 		return "EmployeeListForm";
 	}
@@ -46,7 +46,7 @@ public class EmployeeController {
 	public String editEmp(@RequestParam int id, Model model) {
 
 		model.addAttribute("eModel", empService.getEmpById(id));
-
+		model.addAttribute("dList", deptService.getAllDepts());
 		return "EmployeeEditForm";
 	}
 
