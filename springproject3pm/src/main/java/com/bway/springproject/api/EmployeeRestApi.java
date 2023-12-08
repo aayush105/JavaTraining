e5b480b45ee0f814299282df37712a1912ccaca2
aayush105 <sthaaayush105@gmail.com>
2023-12-08 20:01:30 +0545

    add a book-course-application full backend

--- springproject3pm/src/main/java/com/bway/springproject/api/EmployeeRestApi.java
+++ springproject3pm/src/main/java/com/bway/springproject/api/EmployeeRestApi.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.ConvertingComparator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +41,7 @@ public class EmployeeRestApi {
 		return empService.getEmpById(id);
 	}
 
-	@GetMapping("/api/emp/add")
+	@PostMapping("/api/emp/add")
 	public String add(@RequestBody Employee emp) {
 
 		empService.addEmp(emp);
